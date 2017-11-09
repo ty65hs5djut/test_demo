a52ffbea64b99856106b6067bfa58fc87ec93ec6
汤柏林 <code_man@yeah.net>
2017-11-09 17:16:23 +0800

    book
    this is book

---
+++ MLLabel.h
@@ -0,0 +1,15 @@
+//
+//  MLLabel.h
+//  xmpp_copy
+//
+//  Created by 汤柏林 on 2017/10/16.
+//  Copyright © 2017年 汤柏林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLLabel : UILabel
+
+
+
+@end
---
+++ MLLabel.m
@@ -0,0 +1,20 @@
+//
+//  MLLabel.m
+//  xmpp_copy
+//
+//  Created by 汤柏林 on 2017/10/16.
+//  Copyright © 2017年 汤柏林. All rights reserved.
+//
+
+#import "MLLabel.h"
+#import "GCDMulticastDelegate.h"
+
+@implementation MLLabel{
+    
+}
+
+- (void)setText:(NSString *)text{
+    
+}
+
+@end
